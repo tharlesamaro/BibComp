ed46a190e78988b44d937db7a0007f84e27ed93d
Tharles Amaro <tharlesamaro@gmail.com>
2018-10-18 00:01:33 -0300

    feito algumas configurações do Header

--- App.js
+++ App.js
@@ -2,8 +2,28 @@ import { createStackNavigator } from 'react-navigation';
 
 import LoginPage from './src/pages/LoginPage';
 
-export default createStackNavigator({
-  'Login': {
-    screen: LoginPage
+export default createStackNavigator(
+  {
+    'Login': {
+      screen: LoginPage,
+      navigationOptions: {
+        title: "BibComp - Login"
+      }
+    }
+  },
+  {
+    navigationOptions: {
+      title: "BibComp",
+      headerTintColor: 'white',
+      headerStyle: {
+        backgroundColor: '#00adb5',
+        borderBottomWidth: 1,
+        borderBottomColor: '#eeeeee',
+      },
+      headerTitleStyle: {
+        color: 'white',
+        fontSize: 25,
+      }
+    }
   }
-})
+);
