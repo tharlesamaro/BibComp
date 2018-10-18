d9f2cec46f7186a8fceea0daf43ea20599734e1a
Tharles Amaro <tharlesamaro@gmail.com>
2018-10-17 22:53:42 -0300

    Iniciando a página de login

--- App.js
+++ App.js
@@ -1,21 +1,9 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+import LoginPage from './src/pages/LoginPage';
+
+export default createStackNavigator({
+  'Login': {
+    screen: LoginPage
   }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+})
